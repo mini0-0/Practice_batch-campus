7f0a57143d42d04fca97da2d7c0bf7d07811cec5
mini0-0 <nalala8200@naver.com>
2025-02-12 14:23:39 +0900

    # 1 - 초기 세팅
    Spring Boot 초기 환경 설정

---
+++ src/test/java/com/example/batchcampus/BatchCampusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.batchcampus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BatchCampusApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
