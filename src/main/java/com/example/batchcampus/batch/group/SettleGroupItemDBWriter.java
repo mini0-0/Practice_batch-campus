82f012ef95efda7547afa53263cb2c42f1ad8294
mini0-0 <nalala8200@naver.com>
2025-02-13 19:18:21 +0900

    # 5 - 주간 정산 배치 생성
    일주일 중 금요일을 기준으로 정산 배치 생성

---
+++ src/main/java/com/example/batchcampus/batch/group/SettleGroupItemDBWriter.java
@@ -0,0 +1,29 @@
+package com.example.batchcampus.batch.group;
+
+
+import com.example.batchcampus.domain.SettleGroup;
+import com.example.batchcampus.domain.repository.SettleGroupRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.batch.item.Chunk;
+import org.springframework.batch.item.ItemWriter;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+@RequiredArgsConstructor
+public class SettleGroupItemDBWriter implements ItemWriter<List<SettleGroup>> {
+
+    private final SettleGroupRepository settleGroupRepository;
+
+    @Override
+    public void write(Chunk<? extends List<SettleGroup>> chunk) throws Exception {
+        final List<SettleGroup> settleGroups = new ArrayList<>();
+
+        chunk.forEach(settleGroups::addAll);
+
+        settleGroupRepository.saveAll(settleGroups);
+    }
+
+}
